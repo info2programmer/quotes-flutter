98b871c1873c0f1ab0acda2f0ec26e9493fbb4d1
Saikat Bhadury <info2programmer@gmail.com>
2022-04-06 21:03:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'quote.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home : QuoteList()
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({Key? key}) : super(key: key);
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<Quote> quotes = [
+    Quote(text: "Be yourself; everyone else is already taken.", by :"Oscar Wilde"),
+    Quote(text: "wo things are infinite: the universe and human stupidity; and I\'m not sure about the universe.", by :"Albert Einstein"),
+    Quote(text: "So many books, so little time", by :"Frank Zappa")
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      backgroundColor: Colors.grey[800],
+      appBar:  AppBar(
+        title: const Text(
+            "Awesome Quotes",
+          style: TextStyle(
+            fontFamily: 'Grape',
+            fontSize: 30.0
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0.5,
+        backgroundColor: Colors.grey[800],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: quotes.map((e) => Text(
+            '${e.text} - ${e.by}',
+          style:  TextStyle(
+            fontFamily: 'Lato',
+            fontSize: 20.0,
+            color: Colors.grey[200]
+          ),
+        )).toList(),
+      ),
+    );
+  }
+}
+
+
---
+++ lib/quote.dart
@@ -0,0 +1,6 @@
+class Quote {
+  String by;
+  String text;
+
+  Quote({ required this.by, required this.text });
+}
